56566ec84c0ad08bca6f3dfa5bf825e02479063f
Golder12 <golderalikaya@gmail.com>
2022-07-06 15:05:29 +0300

    changing size of image

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,39 @@
+import tensorflow as tf
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+
+model = tf.keras.models.load_model('sickNormalModel.h5')
+
 import streamlit as st
-import streamlit_authenticator as stauth
+st.write("""
+         # Chest Opacity Classification
+         """
+         )
+st.write("This is a simple image classification web app to diagnose chest opacities in patients")
+file = st.file_uploader("Please upload an image file", type=["jpg", "png", "jpeg"])
 
-with open('../credentials.yaml') as file:
-	config = yaml.load(file, Loader=SafeLoader)
-	
-authenticator = Authenticate(config['credentials'],config['cookie']['stayLoggedIn'],config['cookie']['5l99yd'],config['cookie']['1'],config['preauthorized'])
+def import_and_predict(image_data, model):
+    
+        
+        img = cv2.resize(np.float32(image_data),(96,122))
+        image = img.reshape([-1,96,122,1])
+        prediction = model.predict(image)
+        return prediction
 
-name, authentication_status, username = authenticator.login('Login','main')
-
-url = 'https://golder12-chest-opacity-streamlit-rps-app-sicknormal-v647t5.streamlitapp.com/'
-
-if authentication_status:
-	webbrowser.open_new_tab(url)
-	
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    new_image = image.resize((600, 400))
+    st.image(new_image)
+    prediction = import_and_predict(image, model)
+    st.write("DIAGNOSIS:")
+    if np.argmax(prediction) == 0:
+        st.write("HEALTHY")
+    else:
+        st.write("SICK")
+    
+    
+    #st.text("Probability (0: Normal, 1: Sick")
+    #st.write(prediction)
